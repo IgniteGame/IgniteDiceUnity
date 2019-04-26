808d6103a1bad765f9c21025b9ab7c44f269add8
RGB Studios <justingolden21@gmail.com>
2019-04-26 02:45:47 -0700

    init

---
+++ Assets/Scripts/Roll.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Roll : MonoBehaviour {
+
+    public float startX;
+
+    void Start() {
+        DoRoll();
+    }
+
+    void Update() {
+        if(Input.GetKeyDown("space") || Input.GetMouseButtonDown(0) ) {
+            DoRoll();
+        }
+        if (Input.GetKeyDown("escape") ) {
+            Application.Quit();
+        }
+    }
+
+    public void DoRoll() {
+        transform.position = new Vector3(startX, 2, 0);
+        transform.rotation = Random.rotation;
+        Rigidbody rb = GetComponent<Rigidbody>();
+        rb.velocity = new Vector3(Random.Range(-3, 3), Random.Range(-10, 5), Random.Range(-5, 0) );
+    }
+
+}
