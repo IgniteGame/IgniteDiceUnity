c646305f5b53db7d2d5bd97d7af7391f80413ebf
Justin Golden <justingolden21@gmail.com>
2019-05-03 13:25:56 -0700

    added icons, added mute functionality

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -6,12 +6,21 @@ public class AudioManager : MonoBehaviour {
 
     AudioSource audio;
 
+    private bool isMuted = false;
+
     void Start() {
         audio = GetComponent<AudioSource>();
     }
     void Update() {
         if (Input.GetKeyDown("space") || Input.GetMouseButtonDown(0) ) {
-            audio.Play(0);
+            if(!isMuted) {
+                audio.Play(0);
+            }
         }
     }
+
+    public void Mute() {
+        isMuted = true;
+    }
+
 }
